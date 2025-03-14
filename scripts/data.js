0670ffa4d87f3dc589caa0e9e0060fb7f168ae56
darkjenso <darkjenso@gmail.com>
2025-03-14 10:15:52 +0000

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -1,1 +1,20 @@
+export function getCharacterData() {
+    let characters = game.actors
+        .filter(actor => actor.hasPlayerOwner) // Only include player-controlled characters
+        .map(actor => ({
+            name: actor.name,
+            maxHP: actor.system.attributes.hp.max,
+            currentHP: actor.system.attributes.hp.value,
+            AC: actor.system.attributes.ac.value,
+            abilityScores: {
+                STR: actor.system.abilities.str.value,
+                DEX: actor.system.abilities.dex.value,
+                CON: actor.system.abilities.con.value,
+                INT: actor.system.abilities.int.value,
+                WIS: actor.system.abilities.wis.value,
+                CHA: actor.system.abilities.cha.value
+            }
+        }));
 
+    return characters;
+}
